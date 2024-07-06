6dfc3b0d55332060a58b65f8febdd5139266dfca
Dmitrii Kochkaev <128289789+kochkaev@users.noreply.github.com>
2024-07-06 11:12:47 +0700

    Add Event service

---
+++ src/main/java/ru/kochkaev/Seasons4Fabric/Service/Event.java
@@ -0,0 +1,11 @@
+public class Event {
+    
+    public static void registerMethod(EventObject event, Method method) {
+        event.addMethod(method);
+    }
+
+    public static void registerEvent(EventObject event) {
+
+    }
+
+}
