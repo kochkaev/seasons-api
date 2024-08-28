2ea9a8c478f0984634a993a6cc1164b15707aae9
kochkaev <edukisel@yandex.ru>
2024-08-28 14:52:05 +0700

    Update Tasks Now key can be any Object, lambda expression and list of args for them is now saving to new Map1Key2Values collector.

--- src/main/java/ru/kochkaev/api/seasons/service/Task.java
+++ src/main/java/ru/kochkaev/api/seasons/service/Task.java
@@ -1,33 +1,31 @@
 package ru.kochkaev.api.seasons.service;
 
 import ru.kochkaev.api.seasons.util.functional.IFuncRet;
+import ru.kochkaev.api.seasons.util.map.Map1Key2Values;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Task {
 
-    private static final List<IFuncRet> tasks = new ArrayList<>();
-    private static final Map<IFuncRet, List<Object>> tasksMap = new HashMap<>();
-    private static List<IFuncRet> forRemove = new ArrayList<>();
+    private static final Map1Key2Values<Object, IFuncRet, List<Object>> tasks = new Map1Key2Values<>();
+    private static final List<Object> forRemove = new ArrayList<>();
 
+    public static void addTask(Object key, IFuncRet task, List<Object> args) {
+        tasks.put(key, task, args);
+    }
     public static void addTask(IFuncRet task, List<Object> args) {
-        tasks.add(task);
-        tasksMap.put(task, args);
+        addTask(task, task, args);
     }
     public static void removeTask(IFuncRet task) {
         forRemove.add(task);
     }
 
     public static void runTasks() {
-        for (IFuncRet task : tasks) {
-            tasksMap.put(task, task.function(tasksMap.get(task)));
+        for (IFuncRet task : tasks.getFirstValuesSet()) {
+            tasks.setSecond(task, task.function(tasks.getSecond(task)));
         }
-        for (IFuncRet task : forRemove) {
-            tasks.remove(task);
-            tasksMap.remove(task);
+        for (Object key : forRemove) {
+            tasks.remove(key);
         }
         forRemove.clear();
     }
---
+++ src/main/java/ru/kochkaev/api/seasons/util/map/Map1Key2Values.java
@@ -0,0 +1,83 @@
+package ru.kochkaev.api.seasons.util.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Map1Key2Values <K, F, S> {
+
+    private Map<K,ValuesObject> map = new HashMap<>();
+
+    public Map1Key2Values() {
+
+    }
+
+    public void put(K key, F firstValue, S secondValue) {
+        map.put(key, new ValuesObject(firstValue, secondValue));
+    }
+    public void putFrom(Map1Key2Values<K, F, S> from) {
+        map.putAll(from.map);
+    }
+    public void remove(K key) {
+        map.remove(key);
+    }
+
+    public F getFirst(K key) {
+        return map.get(key).getFirst();
+    }
+    public S getSecond(K key) {
+        return map.get(key).getSecond();
+    }
+
+    public void setFirst(K key, F firstValue) {
+        map.get(key).setFirst(firstValue);
+    }
+    public void setSecond(K key, S secondValue) {
+        map.get(key).setSecond(secondValue);
+    }
+
+    public Set<K> getKeySet() {
+        return map.keySet();
+    }
+    public Set<F> getFirstValuesSet() {
+        return map.values().stream().map(ValuesObject::getFirst).collect(Collectors.toSet());
+    }
+    public Set<S> getSecondValuesSet() {
+        return map.values().stream().map(ValuesObject::getSecond).collect(Collectors.toSet());
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public Map1Key2Values<K, F, S> copy() {
+        Map1Key2Values<K, F, S> copy = new Map1Key2Values<>();
+        for (K key : map.keySet()) {
+            copy.put(key, map.get(key).getFirst(), map.get(key).getSecond());
+        }
+        return copy;
+    }
+
+    private class ValuesObject {
+        private F first;
+        private S second;
+        public ValuesObject(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+        public F getFirst() {
+            return first;
+        }
+        public void setFirst(F first) {
+            this.first = first;
+        }
+        public S getSecond() {
+            return second;
+        }
+        public void setSecond(S second) {
+            this.second = second;
+        }
+    }
+
+}
